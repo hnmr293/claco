99ad786b2fd1fc61bcddb6f3df66a16759b3be19
hnmr293 <hnmr293@gmail.com>
2025-03-24 19:54:17 +0900

    add class AsyncClaudeMessageQueue

--- src/claco/chat.py
+++ src/claco/chat.py
@@ -1,7 +1,7 @@
 from claco.sender import Sender
 from claco.receiver import UDPReceiver
 from claco.config import CLACO_UDP_ADDR, CLACO_UDP_PORT, CLACO_SENDER_PATH
-from claco.msg import MessageQueue
+from claco.msg import MessageQueue, AsyncMessageQueue
 
 
 class ClaudeMessageQueue(MessageQueue):
@@ -12,6 +12,19 @@ class ClaudeMessageQueue(MessageQueue):
     def receive_all(self):
         while True:
             msg = self.receive()
+            if msg.strip() == self.exit_tag:
+                break
+            yield msg
+
+
+class AsyncClaudeMessageQueue(AsyncMessageQueue):
+    def __init__(self, maxsize=1, exit_tag="<exit>"):
+        super().__init__(maxsize)
+        self.exit_tag = exit_tag
+
+    async def receive_all(self):
+        while True:
+            msg = await self.receive()
             if msg.strip() == self.exit_tag:
                 break
             yield msg
--- src/claco/msg.py
+++ src/claco/msg.py
@@ -1,24 +1,47 @@
-from queue import Queue, Empty as QueueEmpty
+import queue
+from asyncio import queues as aqueue, sleep as asleep
 import time
 
 
 class MessageQueue:
     def __init__(self, maxsize=1):
-        self._q = Queue(maxsize=maxsize)
+        self._q = queue.Queue(maxsize=maxsize)
         self._closed = False
 
-    def post(self, message):
+    def post(self, message: str) -> None:
         self._q.put(message)
 
-    def receive(self):
+    def receive(self) -> str:
         while True:
             try:
                 return self._q.get_nowait()
-            except QueueEmpty:
+            except queue.Empty:
                 time.sleep(0.05)
 
-    def try_receive(self):
+    def try_receive(self) -> str | None:
         try:
             return self._q.get_nowait()
-        except QueueEmpty:
+        except queue.Empty:
+            return None
+
+
+class AsyncMessageQueue:
+    def __init__(self, maxsize=1):
+        self._q = aqueue.Queue(maxsize)
+        self._closed = False
+
+    async def post(self, message: str) -> None:
+        await self._q.put(message)
+
+    async def receive(self) -> str:
+        while True:
+            try:
+                return await self._q.get()
+            except aqueue.QueueEmpty:
+                await asleep(0.05)
+
+    async def try_receive(self) -> str | None:
+        try:
+            return await self._q.get_nowait()
+        except aqueue.QueueEmpty:
             return None
